6b778b4e1b662138d12442f346b2ac88b5882883
ymen2000 <aymen.nouari@univ-bba.dz>
2023-05-09 22:04:46 +0100

    navigation bar

---
+++ index.js
@@ -0,0 +1,8 @@
+let searchArea = document.querySelector(".searchArea");
+let searchBorder = document.querySelector(".searchBorder");
+
+
+searchArea.addEventListener("click", ()=>{
+    searchBorder.classList.add("shadowed")
+})
+  
